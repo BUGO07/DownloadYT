03fa4e00296422ded6076bbf7e65286f57c4a44d
BUGO07 <grigalagio07@gmail.com>
2023-01-29 15:13:26 +0400

    finish

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { qualityType, urlType } from "downloadyt";
+import prompts from "prompts";
+import { downlaod } from "./download";
+
+function validateURL(url: urlType) {
+  return /[a-zA-Z0-9-_]{11}$/.test(url);
+}
+
+async function main() {
+  const { url } = await prompts({
+    type: "text",
+    name: "url",
+    message: "Enter YouTube URL\n",
+    validate: (value: urlType) =>
+      validateURL(value) ? true : `Please specify YouTube URL`,
+  });
+  const { quality } = await prompts({
+    type: "text",
+    name: "quality",
+    message:
+      "Enter Desired Video Quality\n(highest | lowest | highestaudio | lowestaudio | highestvideo | lowsetvideo)\n",
+    validate: (value: string) => {
+      return [
+        "highest",
+        "lowest",
+        "highestaudio",
+        "lowestaudio",
+        "highestvideo",
+        "lowsetvideo",
+      ].includes(value)
+        ? true
+        : `Please specify desired video quality.`;
+    },
+  });
+
+  console.time("Downloaded in");
+  const path = await downlaod(url, quality);
+  console.timeEnd("Downloaded in");
+  console.log(`Downloaded to ${path}`);
+}
+
+main();
---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+declare module "downloadyt" {
+  type urlType = string;
+  type qualityType =
+    | "lowest"
+    | "highest"
+    | "highestaudio"
+    | "lowestaudio"
+    | "highestvideo"
+    | "lowestvideo";
+}
